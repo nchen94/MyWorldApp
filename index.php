abe7760347b8b0a3c1727aa4a018a1eea19f94ea
sabrina-gisselle <sabrina.gisselle@ufl.edu>
2014-04-07 23:54:08 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+#!/usr/local/bin/php
+
+
+<html>
+ <head>
+  <title>
+   Treasure Finder
+  </title>
+ </head>
+ <body>
+  <form name='form' method='post' action='test.php'>
+   Username: <input type = "Text" value = "" name = "un" id="un">
+   Password: <input type = "password" value="" name= "pw" id="pw">
+   <input type = "Submit" value = "Login">
+  </form>
+  <form name='form' method='post' action='signup.php'>
+   <p>Don't have an account?
+    <input type="Submit" value="Signup">
+   </p>
+  </form> 
+ </body>
+</html>
