cba9489d2c7074884cdd6e0db98b7180be848a16
davidgithub614 <davidy114@hotmail.com>
2014-04-08 20:19:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
   </title>
  </head>
  <body>
-  <form name='form' method='post' action='profile.php'>
-   Username: <input type = "Text" value = "" name = "un" id="un">
-   Password: <input type = "password" value="" name= "pw" id="pw">
+  <form name = 'form' method = 'post' action = 'profile.php'>
+   Username: <input type = "Text" value = "" name = "un" id = "un">
+   Password: <input type = "password" value="" name = "pw" id = "pw">
    <input type = "Submit" value = "Login">
   </form>
-  <form name='form' method='post' action='signup.php'>
+  <form name = 'form' method = 'post' action = 'signup.php'>
    <p>Don't have an account? 
-    <input type="Submit" value="Signup">
+    <input type = "Submit" value = "Signup">
    </p>
   </form> 
  </body>
