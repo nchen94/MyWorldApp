7d0f4a2ff82c332c8f8d3f2956d3c6a741f02d34
davidgithub614 <davidy114@hotmail.com>
2014-04-08 21:56:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,18 @@
   <title>
    Treasure Hunters
   </title>
+  <style>
+     h {
+     	text-align: center; 
+     	text-transform: capitalize;
+     	font: Georgia, serif;
+      font-size: 2.5em; 
+     	color: #996600;
+     }      
+  </style>
  </head>
  <body>
+  <h>Treasure Hunters</h>
   <form name = 'form' method = 'post' action = 'profile.php'>
    Username: <input type = "Text" value = "" name = "un" id = "un">
    Password: <input type = "password" value="" name = "pw" id = "pw">
